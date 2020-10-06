e730e269c6a70124b63a91896eb3845cbdbf8db0
Aleksander Czeszyk <aleksander.czeszyk@intive.com>
2020-10-06 12:11:59 +0200

    updated test

--- gps_tracker/tests.py
+++ gps_tracker/tests.py
@@ -28,6 +28,7 @@ class TestRoute(TestCase):
         self.route_post = requests.post(ROUTE_ENDPOINT)
         route = self.route_post.json()
         route_id = route['route_id']
+        print(route_id)
         self._push_route(route_id)
         self.length_get = requests.get(ROUTE_LENGTH_ENDPOINT.format(route_id))
 
@@ -41,8 +42,19 @@ class TestRoute(TestCase):
         for coordinates in coords:
             requests.post(ROUTE_ADD_WAY_POINT_ENDPOINT.format(route_id), json=coordinates)
 
-    def _get_route(self, pk):
+    def _create_route_with_chosen_date(self, date):
+        route = Route()
+        route.save()
+        pk = route.pk
+        Route.objects.filter(pk=pk).update(date=date)
         return Route.objects.get(pk=pk)
+
+    def _create_point_with_chosen_date(self, lon, lat, route, date):
+        point = GeoPoint(lon=lon, lat=lat, route=route)
+        point.save()
+        pk = point.pk
+        GeoPoint.objects.filter(pk=pk).update(date=date)
+        return GeoPoint.objects.get(pk=pk)
 
     def test_length_calculation(self):
         length = self.length_get.json()
@@ -50,45 +62,37 @@ class TestRoute(TestCase):
 
     def test_returning_longest_route_per_date(self):
         yesterday = datetime.today().date() - timedelta(days=1)
-        yesterday = yesterday.strftime('%Y-%m-%d')
+
         routes = []
         for i in range(3):
-            routes.append(Route(date=yesterday))
-            routes[i].save()
-
-        print(routes)
+            routes.append(self._create_route_with_chosen_date(date=yesterday))
 
         for coord in self.wgs84_coordinates:
             for route in routes:
-                model = GeoPoint(date=yesterday, lon=coord["lon"], lat=coord["lat"], route=route)
-                model.save()
+                self._create_point_with_chosen_date(lon=coord["lon"], lat=coord["lat"], route=route, date=yesterday)
 
-        route_2_additional_point = GeoPoint(
-            date=yesterday,
+        # create point for middle length route
+        self._create_point_with_chosen_date(
             lon=self.additional_coords[0]["lon"],
             lat=self.additional_coords[0]["lat"],
-            route=routes[1])
-        route_2_additional_point.save()
+            route=routes[1],
+            date=yesterday
+        )
 
-        route_3_additional_points = [
-            GeoPoint(
-                date=yesterday,
-                lon=self.additional_coords[0]["lon"],
-                lat=self.additional_coords[0]["lat"],
-                route=routes[2]
-            ),
-            GeoPoint(
-                date=yesterday,
-                lon=self.additional_coords[1]["lon"],
-                lat=self.additional_coords[1]["lat"],
-                route=routes[2]
-            )
-        ]
-        for point in route_3_additional_points:
-            point.save()
+        # create points for longest route
+        for coord in self.additional_coords:
+            self._create_point_with_chosen_date(lon=coord["lon"], lat=coord["lat"], route=routes[2], date=yesterday)
 
         longest_route_per_day_response = requests.get(LONGEST_ROUTE_PER_DAY_ENDPOINT).json()
 
-        assert routes[2].route_id in longest_route_per_day_response[yesterday]
+        yesterday = yesterday.strftime("%Y-%m-%d")
+
+        result = ''
+
+        for longest_route_per_day_object in longest_route_per_day_response:
+            if longest_route_per_day_object["date"] == yesterday:
+                result += yesterday
+
+        assert result
         print(longest_route_per_day_response)
 
